6a36bc3427800e5c99e0cc95287ea5f39f29a760
Tomleb987 <142760139+Tomleb987@users.noreply.github.com>
2025-06-22 10:34:42 -1000

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,11 +10,20 @@ export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
 
   useEffect(() => {
+    // Gestion récupération mot de passe
     supabase.auth.onAuthStateChange(async (event, session) => {
       if (event === "PASSWORD_RECOVERY") {
         router.push('/reset-password');
       }
     });
+
+    // Script Plausible Analytics
+    const script = document.createElement("script");
+    script.defer = true;
+    script.dataset.domain = "fenuasim.com";
+    script.dataset.api = "https://plausible.io/api/event";
+    script.src = "https://plausible.io/js/script.js";
+    document.head.appendChild(script);
   }, [router]);
 
   return (
