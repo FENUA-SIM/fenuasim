10675418367e027a009f23b99978a705ddc73225
Abdul Muiz <abdulmuizpk@gmail.com>
2025-08-04 04:13:10 +0500

    Update topup_success.tsx

--- src/pages/checkout/topup_success.tsx
+++ src/pages/checkout/topup_success.tsx
@@ -64,7 +64,7 @@ export default function SuccessPage() {
         const { data: esimData, error: esimError } = await supabase
           .from("airalo_topups")
           .select("*")
-          .eq("package_id", orderData.airalo_order_id)
+          .eq("package_id", orderData.airalo_order_id.split("-topup")[0])
           .order("created_at", { ascending: false })
           .limit(1)
           .maybeSingle();
